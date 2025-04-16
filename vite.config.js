26fb915da20b25ada7ceeb317ab957cb1ffe4c53
Tommy <tomasmelesse413@gmail.com>
2025-04-16 20:05:11 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/itachiuchiha/',  // Must match your repo name
   plugins: [react()],
 })
+
